17cb923c982ce5299236a6ab9dd2bf41cb7f014c
Erik Dever <erikdever@hotmail.com>
2022-05-30 19:20:46 -0600

    initial commit

---
+++ node-files/step1.js
@@ -0,0 +1,13 @@
+fs = require('fs')
+
+function cat(path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.log('Error...', err)
+            process.exit(1)
+        }
+        console.log(data)
+    })
+}
+
+cat(process.argv[2]);
